a1edf70c84b747b939adb0cc4a8e13ae17d762ce
xWafl <73sampleperson@gmail.com>
2021-05-24 16:42:36 -0400

    feat: start level selector

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "raylib.h"
-#include <stdlib.h>
 #include <iostream>
+#include <fstream>
+#include <filesystem>
 #include <string>
 #include <array>
 #include <vector>
@@ -27,10 +28,11 @@ constexpr auto SCREEN_HEIGHT = 450;
 enum Scene
 {
     Game,
-    LevelEditor
+    LevelEditor,
+    LevelSelect
 };
 
-Scene gameScene = LevelEditor;
+Scene gameScene = LevelSelect;
 
 enum EnvItemType {
     Nonsolid,
@@ -117,7 +119,8 @@ typedef struct Player {
 
 Player player = { 0 };
 
-Rectangle editorPreviewRectangle = {10, 10, 100, 20 };
+Rectangle editorPreviewRectangle = {10, 10, 100, 30 };
+Rectangle editorLevelnameRectangle = { SCREEN_WIDTH - 110, 10, 100, 30 };
 
 std::vector<EnvItem> envItems = {
     {{ 290, 440, 20, 20 }, EnvItemType::Crystal },
@@ -131,6 +134,8 @@ std::vector<EnvItem> envItems = {
     {{ 600, 580, 200, 20 }, EnvItemType::Hazard, { RED } },
     {{ -1000, 1000, 3000, 20 }, EnvItemType::Hazard, { RED } }
 };
+
+std::vector<char> levelName;
 
 void OnDeath(Player& p)
 {
@@ -249,6 +254,14 @@ CollisionResult CollisionFinnaHappen(EnvItem& ei, int envIndex, float delta)
 
 void UpdatePlayer(float delta)
 {
+    if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
+    {
+        if (CheckCollisionPointRec(GetMousePosition(), { 10, 10, 100, 30 }))
+        {
+            gameScene = LevelSelect;
+        }
+    }
+
     player.frameCounter++;
 
     if (player.frameCounter >= (60/8))
@@ -471,6 +484,55 @@ void LoadEditorLevel(Camera2D& camera)
             }
         }
     }
+
+    std::string levelStr;
+
+    for (char i : levelName)
+    {
+        levelStr += i;
+    }
+
+    std::ofstream levelFile;
+    levelFile.open(SAVES_PATH + levelStr + ".txt");
+    levelFile << levelEditor.dump();
+    levelFile.close();
+}
+
+void LoadLevelFile(const std::string& file)
+{
+    std::cout << "Loading " << file << ".txt" << std::endl;
+
+    std::string fileContent;
+    std::getline(std::ifstream(SAVES_PATH + file + ".txt"), fileContent, '\0');
+
+    std::cout << fileContent << std::endl;
+
+    levelEditor = json::parse(fileContent);
+
+    envItems.clear();
+    for (auto& [x, row] : levelEditor.items())
+    {
+        for (auto& [y, blockType] : row.items())
+        {
+            if (blockType == 1)
+            {
+                envItems.push_back({ { std::stof(x) * 15, std::stof(y) * 15, 15, 15 }, EnvItemType::Solid, { GRAY } });
+            }
+            else if (blockType == 2)
+            {
+                envItems.push_back({ { std::stof(x) * 15, std::stof(y) * 15, 15, 15 }, EnvItemType::Hazard, { RED } });
+            }
+            else if (blockType == 3)
+            {
+                Vector2 newVec = { std::stof(x) * 15, std::stof(y) * 15 };
+                player.respawnPoint = newVec;
+                player.rect.x = newVec.x;
+                player.rect.y = newVec.y;
+            }
+        }
+    }
+
+    gameScene = Game;
 }
 
 void UpdateLevelCamera(Camera2D& camera)
@@ -549,6 +611,38 @@ void UpdateLevelCamera(Camera2D& camera)
     }
 };
 
+void UpdateLevelSelect (const std::filesystem::directory_iterator& saves)
+{
+    std::vector<Rectangle> rects;
+
+    float row = 0.0f;
+    for (const auto& save : saves)
+    {
+        std::cout << save.path().stem() << std::endl;
+        rects.push_back({ 10, 10 + (++row * 50), 100, 30 });
+    }
+
+    Vector2 mousePos = GetMousePosition();
+    if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
+    {
+        if (CheckCollisionPointRec(mousePos, { 10, 10, 100, 30 }))
+        {
+            gameScene = LevelEditor;
+        }
+
+        int saveNum = 0;
+        for (const auto& save : saves)
+        {
+            std::cout << rects[saveNum] << std::endl;
+            if (CheckCollisionPointRec(mousePos, rects[saveNum]))
+            {
+                LoadLevelFile(save.path().stem().string());
+            }
+            saveNum++;
+        }
+    }
+};
+
 int main()
 {
     // Initialization
@@ -609,6 +703,13 @@ int main()
 
     const std::array<const Texture2D*, 4> spikeSprites = { &upSpikes, &rightSpikes, &downSpikes, &leftSpikes };
 
+    const std::filesystem::directory_iterator saves = std::filesystem::directory_iterator(SAVES_PATH);
+
+    for (const auto& save : saves)
+    {
+        std::cout << save.path().stem() << std::endl;
+    }
+
     player.rect = {0, 0, 40, 40 };
     player.speed.x = 0;
     player.speed.y = 0;
@@ -618,8 +719,6 @@ int main()
     player.currentFrame = 0;
     player.frameCounter = 0;
     player.stamina = 110.0f;
-
-    envItems[6].Hazard.direction = Direction::Down;
 
     Camera2D camera = { 0 };
     camera.target = { player.rect.x, player.rect.y };
@@ -642,9 +741,18 @@ int main()
             UpdatePlayer(deltaTime);
             camera.target = { player.rect.x, player.rect.y };
         }
-        else
+        else if (gameScene == LevelEditor)
         {
             UpdateLevelCamera(camera);
+        }
+        else if (gameScene == LevelSelect)
+        {
+            for (const auto& save : saves)
+            {
+                std::cout << save.path().stem() << std::endl;
+            }
+            std::cout << "end" << std::endl;
+            UpdateLevelSelect(saves);
         }
         camera.offset = { SCREEN_WIDTH / 2.0f, SCREEN_HEIGHT / 2.0f };
 
@@ -706,24 +814,26 @@ int main()
                 DrawTexturePro(idleSprites[player.currentFrame % 9], { 7, 18, 14.0f * (player.facingLeft ? -1.0f : 1.0f), 14 }, playerRect, { 0, 0 }, 0, WHITE);
             }
         }
-        else
+        EndMode2D();
+
+        if (gameScene == LevelEditor)
         {
             int boxHeight = 15;
             for (int i = 0; i <= SCREEN_HEIGHT / boxHeight; i++)
             {
-                DrawLine(-SCREEN_WIDTH / 2, -SCREEN_HEIGHT / 2 + i * boxHeight, SCREEN_WIDTH / 2, -SCREEN_HEIGHT / 2 + i * boxHeight, BLACK);
+                DrawLine(0, i * boxHeight, SCREEN_WIDTH, i * boxHeight, BLACK);
             }
             int boxWidth = 15;
             for (int i = 0; i <= SCREEN_WIDTH / boxWidth; i++)
             {
-                DrawLine(-SCREEN_WIDTH / 2 + i * boxWidth, -SCREEN_HEIGHT / 2, -SCREEN_WIDTH / 2 + i * boxWidth, SCREEN_HEIGHT / 2, BLACK);
+                DrawLine(i * boxWidth, 0, i * boxWidth, SCREEN_HEIGHT, BLACK);
             }
 
             for (auto& [x, row] : levelEditor.items())
             {
                 for (auto& [y, blockType] : row.items())
                 {
-                    Rectangle boxRect = { std::stof(x) * boxWidth - camera.offset.x, std::stof(y) * boxHeight - camera.offset.y, static_cast<float>(boxWidth), static_cast<float>(boxHeight) };
+                    Rectangle boxRect = { std::stof(x) * boxWidth, std::stof(y) * boxHeight, static_cast<float>(boxWidth), static_cast<float>(boxHeight) };
                     if (blockType == 1)
                     {
                         DrawRectangleRec(boxRect, GRAY);
@@ -738,15 +848,63 @@ int main()
                     }
                 }
             }
-        }
-        EndMode2D();
 
-        if (gameScene == LevelEditor)
-        {
             DrawRectangleRec(editorPreviewRectangle, RED);
-        }
+            DrawRectangleRec(editorLevelnameRectangle, LIME);
 
-        DrawText(TextFormat("Climbing: %f", player.dashRemaining), 20, 20, 10, DARKGRAY);
+            if (CheckCollisionPointRec(GetMousePosition(), editorLevelnameRectangle))
+            {
+                // Get char pressed (unicode character) on the queue
+                int key = GetKeyPressed();
+
+                // Check if more characters have been pressed on the same frame
+                while (key > 0)
+                {
+                    // NOTE: Only allow keys in range [32..125]
+                    if ((key >= 32) && (key <= 125) && (levelName.size() < 20))
+                    {
+                        levelName.push_back((char)key);
+                    }
+
+                    key = GetKeyPressed();  // Check next character in the queue
+                }
+
+                if (IsKeyPressed(KEY_BACKSPACE) && !levelName.empty())
+                {
+                    levelName.pop_back();
+                }
+            }
+
+            char drawText[21] = "";
+
+            for (int i = 0; i < levelName.size(); i++)
+            {
+                drawText[i] = levelName[i];
+            }
+
+            drawText[levelName.size()] = '\0';
+
+            DrawText(drawText, editorLevelnameRectangle.x, editorLevelnameRectangle.y, 14, BLACK);
+        }
+        else if (gameScene == LevelSelect)
+        {
+            DrawRectangleRec({ 10, 10, 100, 30 }, RED);
+            DrawText("Level editor", 10, 10, 24, BLACK);
+            std::string path = SAVES_PATH;
+            float row = 1.0f;
+            for (const auto& entry : std::filesystem::directory_iterator(path))
+            {
+                DrawRectangleRec({ 10, 10 + (row * 50), 100, 30 }, RED);
+                DrawText(entry.path().stem().string().c_str(), 10, 10 + row * 50, 24, BLACK);
+                row++;
+            }
+        }
+        else if (gameScene == Game)
+        {
+//            DrawText(TextFormat("Climbing: %f", player.dashRemaining), 20, 20, 10, DARKGRAY);
+            DrawRectangleRec({ 10, 10, 100, 30 }, RED);
+            DrawText("Level select", 10, 10, 18, BLACK);
+        }
 
         EndDrawing();
         //----------------------------------------------------------------------------------
